1299c8a75f0f905deebb835f30e3f9952052e197
Alexandru Tenche <alex.tenche@gmail.com>
2016-12-19 23:32:24 +0200

    reformat code

--- delete_contact.php
+++ delete_contact.php
@@ -1,16 +1,15 @@
-<?php include('core/init.php');
+<?php
+include(__DIR__ . '/core/init.php');
 
 $db = new Database;
 
-// run query
 $db->query("DELETE FROM contacts WHERE id = :id");
 
-// bind values
 $db->bind(':id', $_POST['id']);
 
-if($db->execute()){
-	echo $_POST['id'];
-	echo "Contact was deleted";
+if ($db->execute()) {
+    echo $_POST['id'];
+    echo "Contact was deleted";
 } else {
-	echo "Could not delete contact";
+    echo "Could not delete contact";
 }
